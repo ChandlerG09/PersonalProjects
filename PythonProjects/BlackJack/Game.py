e9719c164d1b21a712b8a993ad6b4d553fec301f
Chandler <glowickc@mail.gvsu.edu>
2022-11-04 22:32:47 -0400

    Started implementing hit and stay

--- PythonProjects/BlackJack/Game.py
+++ PythonProjects/BlackJack/Game.py
@@ -51,37 +51,77 @@ def drawCard(tempDeck):
     return tempDeck
 
 def printCard(card):
-    print(str(card.value))
+    print(str(card.strNum))
 
 def findChoices(val1, val2):
-    choices = ['Hit', 'Stay', 'Double']
+    choices = ['hit', 'stay', 'double']
     if val1 == val2:
-        choices.append('Split')
-    pass
+        choices.append('split')
+    return choices
 
-def handleChoice(resp):
-    if resp == 'EXIT':
+def checkSum(tempDeck):
+    sum = 0
+    numAces = 0
+    for card in tempDeck:
+        if card.strNum == 'Ace':
+            numAces+= 1
+        
+        sum += card.value
+    
+    while numAces != 0 and sum > 21:
+        if sum > 21:
+            numAces -= 1
+            sum -= 10
+    
+    return sum
+
+def askPlayer():
+    return input('\nWhat would you like to do? ')
+
+def hit():
+    drawCard(playerHand)
+    if checkSum(playerHand) > 21:
+        print('\nPlayer Busts, Dealer Wins\n\n')
+    else:
+        handleChoice()
+
+def dealerDraw():
+    while checkSum(dealerHand) < 17: #Hit until dealer has at least 17
+        drawCard(dealerHand)
+        if checkSum(dealerHand) - 1 > 21: #soft 17, hit again
+            drawCard(dealerHand)
+
+
+
+def handleChoice():
+    resp = askPlayer().lower()
+
+    if resp.upper() == 'EXIT':
         print('Exiting Game...')
         return EXIT
     
     if resp not in findChoices(playerHand[0].strNum, playerHand[1].strNum):
         print('Sorry, you cannot do that action right now')
     else:
-        pass
+        if resp == 'hit':
+            hit()
+        if resp == 'stay':
+            dealerDraw()
+            
     
 def playGame():
     
     flag = True
 
     while flag:
-        print('Welcome to command line BlackJack!')
-        print('Type EXIT at any point to quit the game')
+        print('\n\nWelcome to command line BlackJack!\n')
+        print('Type EXIT at any point to quit the game\n\n')
         numDecks = input('How many decks of cards would you like to play with? ')
-        if numDecks == 'EXIT':
+        if numDecks.upper() == 'EXIT':
             flag = False
             continue
         
-        print('Creating ' + str(numDecks) + ' deck(s)')
+        print('\nCreating ' + str(numDecks) + ' deck(s)\n')
         createDecks(int(numDecks))
         print('Shuffling the Deck(s)')
         shuffleDeck()
@@ -95,14 +135,18 @@ def playGame():
         while(len(deck) >= threshold):
             
             drawFirstCards()
-            print('Your cards are: ')
+            print('\n\nYour cards are: ')
             for card in playerHand:
                 printCard(card)
 
-            print('Dealer is showing ' + str(dealerHand[0].strNum))
+            print('\nDealer is showing ' + str(dealerHand[0].strNum))
 
             
-            resp = input('What would you like to do? ')
+            
+            n = handleChoice()
+            if n == EXIT:
+                flag = False
+                break
             
             
         
