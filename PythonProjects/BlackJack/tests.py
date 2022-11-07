ee6cf195fd784ba5a99d5b81e220969759e5fba1
Chandler Glowicki <c.g.2000@comcast.net>
2022-11-07 15:07:16 -0500

    Added Test for drawCard

--- PythonProjects/BlackJack/tests.py
+++ PythonProjects/BlackJack/tests.py
@@ -10,8 +10,13 @@ class testCode(unittest.TestCase):
         deck = Game.deck
         self.assertEqual(len(deck), 52)
 
-    def testPullTopCard(self):
-        pass
+    def testDrawCard(self):
+        Game.createDecks(1)
+        deck = Game.deck
+        expected = deck[0]
+        newDeck = Game.drawCard(deck)
+        
+        self.assertEqual(newDeck[-1], expected)
 
 
 if __name__ == '__main__':
